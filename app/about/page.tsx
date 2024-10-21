cbbfc267c50d40a7b8303e327e06acd5da1ba13f
Robotic Reaper <liubaoren2006@gmail.com>
2024-10-21 16:49:26 +0100

    image name

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function AboutPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <main className="container mx-auto max-w-7xl px-6 sm:px-10 md:px-24 flex-grow">
           <div className="flex flex-col lg:flex-row items-center gap-6 justify-center lg:items-start">
-            <div><Image src="Baoren Liu portrait.jpg" width={200} /></div>
+            <div><Image src="Baoren Liu Portrait.jpg" width={200} /></div>
             <div className="w-full lg:w-2/3 text-left">
               I am Baoren Liu, a freshman at <span className="text-red-400">Northeastern University</span>, Boston, currently majoring in <span className="text-blue-400">Computer Science</span>.
               I enjoy making programs to solve random problems.
