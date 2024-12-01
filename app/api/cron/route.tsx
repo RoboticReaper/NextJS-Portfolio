ab7d84b7c451627bdd00cf7a2c8f230165a7a6e3
Robotic Reaper <liubaoren2006@gmail.com>
2024-12-01 15:41:19 +0000

    test

--- app/api/cron/route.tsx
+++ app/api/cron/route.tsx
@@ -4,7 +4,7 @@ import { sql } from '@vercel/postgres';
 export async function GET(request: Request) {
     let epoch = Math.floor(new Date().getTime() / 1000);
     if(request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-        return NextResponse.json({ message: 'unauthorized' }, { status: 401 });
+        return NextResponse.json({ message: 'unauthorized', h: request.headers.get('Authorization') }, { status: 401 });
     } else {
         sql`INSERT INTO clashofclans (name, townhall) VALUES ('test', ${epoch})`
     
