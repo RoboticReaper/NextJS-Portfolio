37c3bbf0a48ee7b45ea9d4543ed41435c6c2d9e4
Robotic Reaper <liubaoren2006@gmail.com>
2024-12-01 18:19:40 +0000

    revert

--- app/api/cron/route.tsx
+++ app/api/cron/route.tsx
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
     } else {
         let cocData = await fetch('https://api.clashofclans.com/v1/players/%23LY8L20QQR', {
             headers: {
-              'Authorization': `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImE1NmI0YjA4LTE1ZjItNGI3OC1hZjQ4LTczYTEyNWY5YzkyMiIsImlhdCI6MTczMTc4Mzk3NCwic3ViIjoiZGV2ZWxvcGVyLzFjN2NkMDhjLWVmNWMtNmRlMy05YzBkLWQyMThkYzJlOTM3NiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc2Ljc2LjIxLjYxIiwiNzYuNzYuMjEuMTIzIl0sInR5cGUiOiJjbGllbnQifV19.iFVLL7deOzv7cl0eGuByhOpfF0UDQEyGgf51XdEIwAX6DQk5WySsJycKeYY7Y-BSNzlnNn2tQ8SHA6_WgjADhA`
+              'Authorization': `Bearer ${process.env.COC_API_KEY}`
             }
         }).then(res => res.json()).then(data => console.log(data))
 
