a1e4d4edbd9d00f8a07b6720d4e64dda4e1c270d
Robotic Reaper <liubaoren2006@gmail.com>
2024-12-01 15:17:39 +0000

    cron test

--- app/api/cron/route.tsx
+++ app/api/cron/route.tsx
@@ -2,10 +2,11 @@ import { NextResponse } from "next/server";
 import { sql } from '@vercel/postgres';
 
 export async function GET(request: Request) {
+    let epoch = Math.floor(new Date().getTime() / 1000);
     if(request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
         return NextResponse.json({ message: 'unauthorized' }, { status: 401 });
     } else {
-        sql`INSERT INTO clashofclans (name, townhall) VALUES ('test', 10)`
+        sql`INSERT INTO clashofclans (name, townhall) VALUES ('test', ${epoch})`
     
         return NextResponse.json({ message: 'success' });
     }
