672e5702bd8cb2be4e0f4705d35bb91284614e77
Robotic Reaper <liubaoren2006@gmail.com>
2024-12-05 15:54:09 +0000

    coc force dynamic again

--- app/api/coc/route.tsx
+++ app/api/coc/route.tsx
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 import { sql } from '@vercel/postgres';
-
+export const dynamic = 'force-dynamic';
 export async function GET(request: Request) {
   const {rows, fields} = await sql`SELECT * FROM clashofclans`
-    
+  
   // cache control for 1 day
   return NextResponse.json({ rows, fields });
 }
