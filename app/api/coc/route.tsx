94620f5c7f93209fbeec0e9a4e38c5359039190d
Robotic Reaper <liubaoren2006@gmail.com>
2024-12-05 16:02:48 +0000

    FUCKIJGN HELL

--- app/api/coc/route.tsx
+++ app/api/coc/route.tsx
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { sql } from '@vercel/postgres';
-export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   const {rows, fields} = await sql`SELECT * FROM clashofclans`
   
