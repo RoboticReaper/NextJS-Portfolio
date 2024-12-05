28ab8a9a30173b2ee8339a0dddfad1eb2a5e2a57
Robotic Reaper <liubaoren2006@gmail.com>
2024-12-05 22:02:30 +0000

    redeployment test

--- app/api/coc/route.tsx
+++ app/api/coc/route.tsx
@@ -11,7 +11,6 @@ export async function GET(request: Request) {
         }
     }).then(res => res.json())
 
-    // cache control for 1 day
     return NextResponse.json(cocData, {
         headers: {
             'Cache-Control': 'no-store', // Ensures the data is always fresh
