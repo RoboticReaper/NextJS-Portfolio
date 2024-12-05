adbd4f7d19771ea8da390ae8186199b36a30b722
Robotic Reaper <liubaoren2006@gmail.com>
2024-12-05 15:35:43 +0000

    maybe shouldn't force dynamic on cron api

--- app/api/coc/route.tsx
+++ app/api/coc/route.tsx
@@ -7,5 +7,5 @@ export async function GET(request: Request) {
   const {rows, fields} = await sql`SELECT * FROM clashofclans`
     
   // cache control for 1 day
-  return NextResponse.json({ rows, fields }, { headers: { 'Cache-Control': 's-maxage=1800' } });
+  return NextResponse.json({ rows, fields });
 }
--- app/api/cron/route.tsx
+++ app/api/cron/route.tsx
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 import { sql } from '@vercel/postgres';
 
-export const dynamic = 'force-dynamic'
-
 export async function GET(request: Request) {
     if(request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
         return NextResponse.json({ message: 'unauthorized'}, { status: 401 });
