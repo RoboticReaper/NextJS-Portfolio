5f90dade6e52795b24675ffaf283b12bb3632765
Robotic Reaper <liubaoren2006@gmail.com>
2024-12-05 16:06:37 +0000

    does no-cache work?

--- app/api/coc/route.tsx
+++ app/api/coc/route.tsx
@@ -2,8 +2,12 @@ import { NextResponse } from "next/server";
 import { sql } from '@vercel/postgres';
 
 export async function GET(request: Request) {
-  const {rows, fields} = await sql`SELECT * FROM clashofclans`
-  
+  const { rows, fields } = await sql`SELECT * FROM clashofclans`
+
   // cache control for 1 day
-  return NextResponse.json({ rows, fields });
+  return NextResponse.json({ rows, fields }, {
+    headers: {
+      'Cache-Control': 'no-store', // Ensures the data is always fresh
+    },
+  });
 }
