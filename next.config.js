3e2b1dacbb4505de1120e65069c240130a00055c
Robotic Reaper <liubaoren2006@gmail.com>
2025-01-21 17:05:32 -0800

    fixed format

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
-import createMDX from '@next/mdx'
- 
-/** @type {import('next').NextConfig} */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/
+});
+
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
-}
- 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-})
- 
+};
+
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+module.exports = withMDX(nextConfig);
